adc337f01b257eac93f52652040163914a9e01e2
Areeba09 <areebawani222@gmail.com>
2023-10-21 20:37:14 +0530

    landingPage

---
+++ chatai/src/Components/DropdownJsx.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { DownOutlined } from "@ant-design/icons";
+import { Dropdown, Space, Typography } from "antd";
+// const items = [
+//   {
+//     key: "1",
+//     label: "Item 1",
+//   },
+//   {
+//     key: "2",
+//     label: "Item 2",
+//   },
+//   {
+//     key: "3",
+//     label: "Item 3",
+//   },
+// ];
+
+const DropdownJsx = ({ selectable, items, className }) => {
+  return (
+    <Dropdown
+      menu={{
+        items,
+        selectable: true,
+        defaultSelectedKeys: ["3"],
+      }}
+    >
+      <Typography.Link>
+        <Space className={className}>
+          {selectable}
+          <DownOutlined />
+        </Space>
+      </Typography.Link>
+    </Dropdown>
+  );
+};
+
+export default DropdownJsx;
--- chatai/src/Components/Home.jsx
+++ chatai/src/Components/Home.jsx
@@ -1,5 +1,11 @@
 import { React, useContext, useState, useEffect } from "react";
-import { Route, Routes, BrowserRouter, useNavigate, Navigate }  from "react-router-dom";
+import {
+  Route,
+  Routes,
+  BrowserRouter,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
 import LeftNav from "../Components/LeftPanel";
 import CenterNav from "../Components/CenterPanel";
 import Settings from "../Components/Settings";
@@ -28,22 +34,23 @@ import AIinput from "./AIinput";
 import AIresponse from "./AIresponse";
 import ThreeSixtyView from "./ThreeSixtyView";
 import EtherConnect from "./EtherConnect";
-import ShareRoomAccess from './ShareRoomAccess'
-import PreSignUp from "./auth/PreSignUp";
+import ShareRoomAccess from "./ShareRoomAccess";
+// import PreSignUp from "./auth/PreSignUp";
 import TaoBaoProductDetails from "./TaoBaoProductDetails";
 import UpWorkJobsSearch from "./UpWorkJobsSearch";
 import GlobalSupplierSearch from "./GlobalSupplierSearch";
-import HomePage from './../Components/HomePage/HomePage'
+import HomePage from "./../Components/HomePage/HomePage";
+import LandingPage from "../Pages/LandingPage";
+import Login from "./auth/Login";
 
 export default function Home() {
-  const {setIsValidKey,isValidKey}=useContext(Context)
-  
+  const { setIsValidKey, isValidKey } = useContext(Context);
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   // const navigate = useNavigate();
   // const token = localStorage.getItem('token')
   // const loggedIn = localStorage.getItem('is_active')
   // setIsLoggedIn(loggedIn)
- 
 
   return (
     <>
@@ -60,6 +67,7 @@ export default function Home() {
                 </>
               }
             />
+            <Route path="/login" element={<Login />} />
             <Route
               exact
               path="/settings"
@@ -85,7 +93,7 @@ export default function Home() {
               path="items/view/:productId"
               element={
                 <>
-                  <ItemDetailView/>
+                  <ItemDetailView />
                 </>
               }
             />
@@ -93,7 +101,7 @@ export default function Home() {
               path="/:id/history/details/:history_id"
               element={
                 <>
-                  <MessageDetailView/>
+                  <MessageDetailView />
                 </>
               }
             />
@@ -107,38 +115,75 @@ export default function Home() {
                 </>
               }
             />
-            {localStorage.getItem('is_active') === 'true' ? (
-                <Route path="/" element={<Navigate to="/room/join" />} />
-              ) : (
-                <Route path="/" element={<PreSignUp />} />
+            {localStorage.getItem("is_active") === "true" ? (
+              <Route path="/" element={<Navigate to="/room/join" />} />
+            ) : (
+              <Route path="/" element={<LandingPage />} />
             )}
 
-            {localStorage.getItem('is_active') === 'true' && (
-            <Route path="/room/join" element={<Room />} />)}
+            {localStorage.getItem("is_active") === "true" && (
+              <Route path="/room/join" element={<Room />} />
+            )}
             <Route path="/:segment1/home/:id" element={<Room />} />
-            <Route path="/:segment1/:id/stocks" element={<Stocks/>} />
-            <Route path="/:segment1/:id/airbnb" element={<AirBnb/>} />
-            <Route path="/:segment1/:id/jobs" element={<SearchJobs/>} />
-            <Route path="/:segment1/:id/upWork-search" element={<UpWorkJobsSearch/>} />
-            <Route path="/:segment1/:id/share-room-access" element={<ShareRoomAccess />} />
+            <Route path="/:segment1/:id/stocks" element={<Stocks />} />
+            <Route path="/:segment1/:id/airbnb" element={<AirBnb />} />
+            <Route path="/:segment1/:id/jobs" element={<SearchJobs />} />
+            <Route
+              path="/:segment1/:id/upWork-search"
+              element={<UpWorkJobsSearch />}
+            />
+            <Route
+              path="/:segment1/:id/share-room-access"
+              element={<ShareRoomAccess />}
+            />
 
             <Route path="/:segment1/:id/jobs/all" element={<Jobs />} />
 
-            <Route path="/:segment1/:id/airbnb/properties" element={<AirBnbProperties/>} />
-            <Route path="/:segment1/:id/currencies" element={<Currencies/>} />
-            <Route path="/:segment1/:id/ai_input" element={<AIinput/>} />
-            <Route path="/:segment1/:id/three_sixty" element={<ThreeSixtyView/>} />
-            <Route path="/:segment1/:id/ether_connect" element={<EtherConnect/>} />
+            <Route
+              path="/:segment1/:id/airbnb/properties"
+              element={<AirBnbProperties />}
+            />
+            <Route path="/:segment1/:id/currencies" element={<Currencies />} />
+            <Route path="/:segment1/:id/ai_input" element={<AIinput />} />
+            <Route
+              path="/:segment1/:id/three_sixty"
+              element={<ThreeSixtyView />}
+            />
+            <Route
+              path="/:segment1/:id/ether_connect"
+              element={<EtherConnect />}
+            />
 
-            <Route path="/:segment1/:id/ai_input/response" element={<AIresponse/>} />
+            <Route
+              path="/:segment1/:id/ai_input/response"
+              element={<AIresponse />}
+            />
 
-            <Route path="/:segment1/:id/global_retailer_handm" element={<GlobalRetailerForm/>} />
-            <Route path="/:segment1/:id/global_retailer_handm/products" element={<GlobalRetailer/>} />
-            <Route path="/:segment1/:id/global_retailer_taobao/products" element={<GlobalRetailer/>} />
-            <Route path="/:segment1/:id/global_retailer_taobao/products/details/:num_id" element={<TaoBaoProductDetails/>} />
-            <Route path="/:segment1/:id/global_suplier_search" element={<GlobalSupplierSearch/>} />
+            <Route
+              path="/:segment1/:id/global_retailer_handm"
+              element={<GlobalRetailerForm />}
+            />
+            <Route
+              path="/:segment1/:id/global_retailer_handm/products"
+              element={<GlobalRetailer />}
+            />
+            <Route
+              path="/:segment1/:id/global_retailer_taobao/products"
+              element={<GlobalRetailer />}
+            />
+            <Route
+              path="/:segment1/:id/global_retailer_taobao/products/details/:num_id"
+              element={<TaoBaoProductDetails />}
+            />
+            <Route
+              path="/:segment1/:id/global_suplier_search"
+              element={<GlobalSupplierSearch />}
+            />
 
-            <Route path="/:segment1/:id/stocks/:symbol/analysis/" element={<StockDetail/>} />
+            <Route
+              path="/:segment1/:id/stocks/:symbol/analysis/"
+              element={<StockDetail />}
+            />
             <Route
               path="/:segment1/:id"
               element={
@@ -157,13 +202,12 @@ export default function Home() {
                 </>
               }
             />
-             <Route
+            <Route
               path="/:segment1/support/room/:id"
               element={
                 <>
-                
-                {isValidKey && <LeftNav/>}
-                 <CustomerSupport/>
+                  {isValidKey && <LeftNav />}
+                  <CustomerSupport />
                 </>
               }
             />
@@ -175,7 +219,7 @@ export default function Home() {
                 </>
               }
             />
-             
+
             <Route path="/supervisor" element={<Supervisor />} />
             <Route path="/supervisor/room/history" element={<RoomHistory />} />
           </Routes>
