9ceec330d9b8187d4ba650608b5108594aceab85
Areeba09 <areebawani222@gmail.com>
2023-10-24 18:58:25 +0530

    signup validations and UI fixes

--- chatai/src/Components/auth/Login.jsx
+++ chatai/src/Components/auth/Login.jsx
@@ -1,23 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { EyeFilled, EyeInvisibleFilled } from "@ant-design/icons";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+  // const [email, setEmail] = useState("");
+  const [loginData, setLoginData] = useState({ email: "", password: "" });
+  // const [password, setPassword] = useState("");
+  const [error, setError] = useState({ email: "", password: "" });
+  // const [passwordError, setPasswordError] = useState("");
   const [passwordType, setPasswordType] = useState(false);
+  const [apiError, setApiError] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
   const validate = () => {
-    !emailRegex.test(email) ? setError("Invalid email!") : setError(null);
-    !password.match(emailRegex)
-      ? setPasswordError("Wrong Password!")
-      : setPasswordError(null);
+    !emailRegex.test(loginData?.email)
+      ? setError((prev) => ({
+          ...prev,
+          email: "Invalid Email",
+        }))
+      : setError((prev) => ({
+          ...prev,
+          email: "",
+        }));
+    !loginData?.password.match(emailRegex)
+      ? setError((prev) => ({
+          ...prev,
+          password: "Wrong Password !",
+        }))
+      : setError((prev) => ({
+          ...prev,
+          password: "",
+        }));
+    loginData?.password.length < 8
+      ? setError((prev) => ({
+          ...prev,
+          password: "Password should be at least 8 characters",
+        }))
+      : setError((prev) => ({
+          ...prev,
+          password: "",
+        }));
   };
   const Login = async (e) => {
     e.preventDefault();
@@ -26,22 +51,17 @@ function Login() {
         // setLoading(true);
         const response = await axios.post(
           "https://chatgptmall.tech/api/v2/login/",
-          {
-            username: email,
-            password: password,
-          }
+          { username: loginData?.email, password: loginData?.password }
         );
         localStorage.setItem("token", response.data.token);
         localStorage.setItem("is_active", response.data.is_active);
         navigate("/room");
-        console.log(response, "res");
       } catch (error) {
         setApiError(error.response.data.non_field_errors);
       }
     }
   };
 
-  const [apiError, setApiError] = useState("");
   // const navigate = useNavigate();
 
   // const handleInputClick = (event, field) => {
@@ -124,14 +144,14 @@ function Login() {
         </div>
       ) : ( */}
       <div className="w-full h-full flex justify-center">
-        <div className="w-fit rounded-xl shadow-lg flex flex-col px-5 py-8 justify-center items-center mt-5">
-          <h3 className="font-Poppins text-3xl font-semibold mb-5 text-textColor ">
+        <div className="w-fit rounded-xl shadow-lg flex flex-col p-5 justify-center items-center mt-20">
+          <h3 className="font-Poppins text-3xl font-semibold mb-3 text-textColor ">
             Login
           </h3>
           {/* {loading && <h4 style={{ color: "white" }}>loading...</h4>} */}
           <p className="text-red-500 font-Poppins text-sm">{apiError}</p>
-          <form className="flex flex-col gap-5">
-            <div className="flex flex-col gap-3">
+          <form className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2">
               <label
                 htmlFor="email"
                 className="font-Poppins text-xl font-medium text-textColor "
@@ -139,19 +159,22 @@ function Login() {
                 Email <span className="text-red-500">*</span>
               </label>
               <input
-                value={email}
+                value={loginData?.email}
                 onBlur={validate}
-                // onChange={handleChange}
                 placeholder="Your Email"
                 className="border-b-2 w-96 outline-none font-Poppins text-lg text-textColor "
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) =>
+                  setLoginData({ ...loginData, email: e.target.value })
+                }
                 required
                 type="email"
                 id="email"
                 name="email"
               />
               {error && (
-                <p className="text-red-500 font-Poppins text-sm ">{error}</p>
+                <p className="text-red-500 font-Poppins text-sm ">
+                  {error?.email}
+                </p>
               )}
             </div>
             <div className="flex flex-col gap-3">
@@ -164,10 +187,12 @@ function Login() {
               <div className="flex relative ">
                 <input
                   onBlur={validate}
-                  value={password}
+                  value={loginData?.password}
                   placeholder="Your Password"
                   className="border-b-2 w-96 outline-none font-Poppins text-lg text-textColor relative "
-                  onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) =>
+                    setLoginData({ ...loginData, password: e.target.value })
+                  }
                   required
                   type={passwordType ? "text" : "password"}
                   id="password"
@@ -181,9 +206,9 @@ function Login() {
                 </button>
               </div>
 
-              {passwordError && (
+              {error && (
                 <p className="text-red-500 font-Poppins text-sm">
-                  {passwordError}
+                  {error?.password}
                 </p>
               )}
             </div>
@@ -191,8 +216,7 @@ function Login() {
               <button
                 type="submit"
                 onClick={Login}
-                // onClick={handleLoginForm}
-                className="rounded-full px-5  bg-gradient-to-r from-cyan-500 to-blue-500 py-2 font-Poppins font-semibold text-xl text-white"
+                className="rounded-full px-5 shadow-md bg-gradient-to-r from-cyan-500 to-blue-500 py-2 font-Poppins font-semibold text-xl text-white"
               >
                 Login
               </button>
