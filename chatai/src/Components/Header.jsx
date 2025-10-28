1c881511f8ace8916bf239f96075431d73b15dd1
FaisalAhmad383 <faisal.ahmad@doctustech.com>
2025-10-28 21:18:26 +0530

    changed to gmall

--- chatai/src/Components/Header.jsx
+++ chatai/src/Components/Header.jsx
@@ -162,7 +162,7 @@ function Header() {
               className="m-0 font-Poppins font-bold cursor-pointer text-3xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%"
               onClick={() => navigate("/")}
             >
-              Homelinked
+              Gmall
             </p>
           </a>
         </div>
